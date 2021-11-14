e49afacb05e13bda49b353235f3134e34ff99874
sajjad <65295591+Sajjad-Mi@users.noreply.github.com>
2021-11-14 18:28:34 +0330

    added page for showing notes

---
+++ public/js/note.js
@@ -0,0 +1,37 @@
+function update(elm){
+    elm.parentElement.parentElement.firstElementChild.style.display="block";
+    elm.parentElement.style.display="none";   
+}      
+ 
+function saveUpdate(formElm){
+    const endpoint='notes';
+    const title = formElm.parentElement.firstElementChild.value;
+    const name = formElm.previousElementSibling.previousElementSibling.previousElementSibling.value;
+    const text = formElm.previousElementSibling.value;
+    fetch(endpoint, {
+        method: 'PUT',
+        body: JSON.stringify({ noteId: formElm.id, title, name, text }),
+        headers: {'Content-Type': 'application/json'}
+    }).then(response => response.json())
+    .then(data=>{
+
+       const noteDiv = formElm.parentElement.nextElementSibling;
+       noteDiv.firstElementChild.innerText=title;
+       noteDiv.firstElementChild.nextElementSibling.innerText=name;
+       noteDiv.firstElementChild.nextElementSibling.nextElementSibling.innerText=text;
+        
+        noteDiv.style.display="block";
+        formElm.parentElement.style.display="none";        
+    })
+    
+    //then(window.location.href="/notes")
+}
+
+function deleteNote(elm){
+    const endpoint = 'deleteNote';
+    fetch(endpoint, {
+        method: 'DELETE',
+        body: JSON.stringify({ noteId: elm.id }),
+        headers: {'Content-Type': 'application/json'}
+    }).then(window.location.href="/notes");
+}
